5960b489b98d455ddf274baef62dfc1a66646768
giorgi94 <giorgik1994@gmail.com>
2018-12-09 22:40:08 +0400

    modified monaco config

--- djangoeditorwidgets/static/djangoeditorwidgets/monaco/monaco.config.js
+++ djangoeditorwidgets/static/djangoeditorwidgets/monaco/monaco.config.js
@@ -1,42 +1,52 @@
 /* global monaco */
 
 document.addEventListener('DOMContentLoaded', function () {
-    require.config({ paths: { 'vs': '/static/monaco/vs' }});
-
-    var container = document.querySelector('textarea[monaco-editor="true"]');
-    var form = container.form;
-
-    var editorWrapper = document.createElement('div');
-    editorWrapper.id = container.id + '--editor';
-    editorWrapper.classList.add('monaco-editor--conteiner');
-
-    require(['vs/editor/editor.main'], function () {
-        try {
-            container.style.display = 'none';
-            container.parentElement.appendChild(editorWrapper);
-
-            var editor = monaco.editor.create(document.getElementById(container.id + '--editor'), {
-                language: container.dataset.language,
-                wordWrap: container.dataset.wordwrap || 'off',
-                minimap: {
-                    enabled: container.dataset.minimap === 'true'
-                },
-                fontSize: 12 + 1,
-                value: container.value
-            });
-
-            window[container.id + '_monaco_editor'] = editor;
-
-            window.addEventListener('resize', function () {
-                editor.layout();
-            });
-
-            form.addEventListener('submit', function (e) {
-                container.value = editor.getValue();
-            });
-        } catch (err) {
-            container.style.display = 'block';
-            editorWrapper.remove();
+    require.config({
+        paths: {
+            'vs': '/static/monaco/vs'
         }
     });
+
+    var containers = Array.from(
+        document.querySelectorAll('textarea[monaco-editor="true"]'));
+
+    containers.forEach(function (container) {
+
+        var form = container.form;
+
+        var editorWrapper = document.createElement('div');
+        editorWrapper.id = container.id + '--editor';
+        editorWrapper.classList.add('monaco-editor--conteiner');
+
+        require(['vs/editor/editor.main'], function () {
+            try {
+                container.style.display = 'none';
+                container.parentElement.appendChild(editorWrapper);
+
+                var editor = monaco.editor.create(document.getElementById(container.id + '--editor'), {
+                    language: container.dataset.language,
+                    wordWrap: container.dataset.wordwrap || 'off',
+                    minimap: {
+                        enabled: container.dataset.minimap === 'true'
+                    },
+                    fontSize: 12 + 1,
+                    value: container.value
+                });
+
+                window[container.id + '_monaco_editor'] = editor;
+
+                window.addEventListener('resize', function () {
+                    editor.layout();
+                });
+
+                form.addEventListener('submit', function (e) {
+                    container.value = editor.getValue();
+                });
+            } catch (err) {
+                container.style.display = 'block';
+                editorWrapper.remove();
+            }
+        });
+    })
+
 });
